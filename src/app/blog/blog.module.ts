71b906f7fdc63e74c3949cf3fd30abfa6d315c2d
kathleeng <kmapsicay@77global.biz>
2022-09-20 17:08:31 +0800

    first commit

---
+++ src/app/blog/blog.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogListComponent } from './pages/blog-list/blog-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    BlogListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
